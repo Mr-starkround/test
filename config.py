75ddc9fb30b4f978f510c38f164cae99cb9795f1
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-06 05:07:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import logging
 
 api_id = int(os.environ.get("API_ID", "21484185"))
 api_hash = os.environ.get("API_HASH", "42589444b3ee86b1286f01d966989214")
-bot_token = os.environ.get("BOT_TOKEN", "6872017467:AAEQAC59qKPm-6ML27z44DjLwZeCzx-wpVk")
+bot_token = os.environ.get("BOT_TOKEN", "6872017467:AAFx_RzH9bGA13iolTAYJNJlDWUfLjQ_C14")
 # =========================================================== #
 
 db_url = os.environ.get("DB_URL", "mongodb+srv://Alexa:alexa@cluster0.h0zqfue.mongodb.net/true?retryWrites=true&w=majority")
