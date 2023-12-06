1a7c2f24eab3ffd4328bd3e70907beb4d0bde959
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-06 12:54:59 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ class Bot(Client):
         self.id_bot = bot_me.id
         data.append(self.id_bot)
         await self.set_bot_commands([
-            BotCommand('status', '📊 check status'), BotCommand('help', '📚bantuan'),
+            BotCommand('status', '📊 check status'), BotCommand('start', '💭kirim menfess'),
         ], BotCommandScopeAllPrivateChats())
         
         print('BOT TELAH AKTIF')
