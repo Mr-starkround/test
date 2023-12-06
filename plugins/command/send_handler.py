74b5813e0b236b9bce819e5e2f82ea43428e39ac
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-06 23:49:35 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -17,7 +17,7 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
                 if coin >= config.biaya_kirim:
                     coin = user.coin - config.biaya_kirim
                 else:
-                    return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali.serta coin mu kurang untuk mengirim menfess diluar batas harian., kamu dapat mengirim menfess kembali pada hari esok.\n\n waktu reset jam 1 pagi. \n\n💰Topup coin silahkan contact @vxnjul', quote=True)
+                    return await msg.reply(f'❌ pesanmu gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali. Coin mu kurang untuk mengirim menfess diluar batas harian. \n\nwaktu reset jam 1 pagi \n\n<b>Kamu dapat mengirim menfess kembali pada esok hari/top up coin untuk mengirim diluar batas harianmu. Topup Coin silahkan hubungi</b> @vxnjul', quote=True)
 
         if key == hastag[0]:
             picture = config.pic_girl
