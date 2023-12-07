5fce113b3b347585b16095925712c8bf259c944d
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-07 19:57:55 +0700

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -190,6 +190,8 @@ async def on_callback_query(client: Client, query: CallbackQuery):
         await photo_handler_inline(client, query)
     elif query.data == 'video':
         await video_handler_inline(client, query)
+    elif query.data == 'ggl':
+        await cb_ggl(client, query)
     elif query.data == 'nsj':
         await cb_help(client, query)
     elif query.data == 'ttp':
