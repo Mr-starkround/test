2327e2db21bf38194823c91a454dd4aae26d0cdf
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-07 16:39:42 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -17,7 +17,7 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
                 if coin >= config.biaya_kirim:
                     coin = user.coin - config.biaya_kirim
                 else:
-                    return await msg.reply(f'❌ pesanmu gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali. Coin mu kurang untuk mengirim menfess diluar batas harian. \n\nwaktu reset jam 1 pagi \n\n<b>Kamu dapat mengirim menfess kembali pada esok hari/top up coin untuk mengirim diluar batas harianmu. Topup Coin silahkan hubungi</b> @vxnjul', quote=True)
+                    return await msg.reply(f'❌ pesanmu gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali. Coin mu kurang untuk mengirim menfess diluar batas harian. \n\nwaktu reset jam 1 pagi \n\n<b>Kamu dapat mengirim menfess kembali pada esok hari/top up coin untuk mengirim diluar batas harianmu. Topup Coin di</b> @topupcoinbot', quote=True)
 
         if key == hastag[0]:
             picture = config.pic_girl
@@ -59,7 +59,7 @@ async def send_menfess_handler(client: Client, msg: types.Message):
                 if coin >= config.biaya_kirim:
                     coin = db_user.coin - config.biaya_kirim
                 else:
-                    return await msg.reply(f'❌ pesanmu gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali. Coin mu kurang untuk mengirim menfess diluar batas harian. \n\nwaktu reset jam 1 pagi \n\n<b>Kamu dapat mengirim menfess kembali pada esok hari/top up coin untuk mengirim diluar batas harianmu. Topup Coin silahkan hubungi</b> @vxnjul', quote=True)
+                    return await msg.reply(f'❌ pesanmu gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali. Coin mu kurang untuk mengirim menfess diluar batas harian. \n\nwaktu reset jam 1 pagi \n\n<b>Kamu dapat mengirim menfess kembali pada esok hari/top up coin untuk mengirim diluar batas harianmu. Topup Coin di</b> @topupcoinbot', quote=True)
 
         link = await get_link()
         kirim = await client.copy_message(config.channel_1, msg.from_user.id, msg.id)
