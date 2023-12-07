9a4ee4fdc9065c3d9c48256f94133c23b0b8d369
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-08 01:19:57 +0700

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -151,7 +151,7 @@ async def on_message(client: Client, msg: Message):
                     return await msg.reply(f'Kamu telah <b>di banned</b>\n\n<u>Alasan:</u> {database.get_data_bot(client.id_bot).ban[str(uid)]}\nsilahkan kontak @vxnjul untuk unbanned', True, enums.ParseMode.HTML)
                 if key in [hastag[0], hastag [1]]:
                     if key == command.lower() or len(command.split(' ')) < 3:
-                        return await msg.reply('config.gagalkirim_msg', True, enums.ParseMode.HTML)
+                        return await config.gagalkirim_msg(client, msg)
                     else:
                         return await send_menfess_handler(client, msg)
                 else:
