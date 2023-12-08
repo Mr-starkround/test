9de138402d5ff8274bbd1ba848e4eab6eb87c8b1
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-08 21:31:08 +0700

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -162,7 +162,8 @@ async def on_message(client: Client, msg: Message):
                         )
                         if key == command.lower()
                         or len(command.split(' ')) < 3
-                        else await send_menfess_handler(client, msg, key, hastag)
+                        else await send_menfess_handler(client, msg, key, hastag) 
+         )
                 elif key in hastag:
                     if key == command.lower() or len(command.split(' ')) < 3:
                         return await msg.reply('🙅🏻‍♀️  post gagal terkirim, <b>mengirim pesan wajib lebih dari 3 kata.</b>', True, enums.ParseMode.HTML)
