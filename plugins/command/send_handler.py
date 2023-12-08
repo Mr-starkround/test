0e9f64b2f26423e1e136ae5337a86f73564c0594
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-08 22:14:33 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -26,9 +26,9 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
                 return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali.serta coin mu kurang untuk mengirim menfess diluar batas harian., kamu dapat mengirim menfess kembali pada hari esok.\n\n waktu reset jam 1 pagi. \n\n\n\n Info: Topup Coin Hanya ke @Anon197', quote=True)
 
         if key == command.lower():
-            key = config.hastag
+            hastag = config.hastag
         elif key == command.lower():
-            key = config.hastag
+            hastag = config.hastag
         
         link = await get_link()
         caption = msg.text or msg.caption
