8ac2374f2c8455cce234070aeccc6de2ddf788a0
Mr-starkround <78058269+Mr-starkround@users.noreply.github.com>
2023-12-08 17:49:06 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -37,7 +37,7 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
 
 async def send_menfess_handler(client: Client, msg: types.Message):
     helper = Helper(client, msg)
-    config = config.hastag(client, msg)
+    hastag = config(client, msg)
     db = Database(msg.from_user.id)
     db_user = db.get_data_pelanggan()
     db_bot = db.get_data_bot(client.id_bot).kirimchannel
